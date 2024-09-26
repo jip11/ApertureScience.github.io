3d919f38d76f53257b76d1b75704d3682dde8343
jip11 <you@example.com>
2024-09-26 13:04:00 +0200

    0926 folyt

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+// Button sound effect (Unused for now)
 const audio = new Audio();
 audio.src = "./audio/Portal_button.mp3";
+
+// users
+document.addEventListener('DOMContentLoaded', function() {
+    var users = {
+        'CJohnson': 'TIER3',
+        'GregS': 'P0ll75'
+    };
+
+// Login script
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (users[username] && users[username] === password) {
+        window.location.href = 'home.html';
+    } else {
+        document.getElementById('errorMessage').textContent = 'Wrong username or password, please try again.';
+    }
+ });
+});
+
