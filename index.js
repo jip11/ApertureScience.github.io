3ba8c7f942f468db7364fe54625fcf1f65e24eca
jip11 <jip463210@gmail.com>
2024-09-26 19:33:25 +0200

    0926 foly foplyt

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Button sound effect (Unused for now)
-const audio = new Audio();
-audio.src = "./audio/Portal_button.mp3";
+
 
 // users
 document.addEventListener('DOMContentLoaded', function() {
@@ -24,3 +22,22 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
  });
 });
 
+// test.html things
+function buffer() {
+    const audio = new Audio();
+    audio.src = "./audio/Portal_button.mp3";
+    audio.play();
+    let fName = document.getElementById('fname') 
+    if(fName.value === ""){
+        alert("FAAAAAAASZ")
+    }
+    else{
+    document.getElementById('form-div').innerHTML = '<img src="./loading-load.gif"/>';
+    setTimeout(()=>{
+        document.getElementById('textcube').innerHTML =
+        '<h1 id="error">ERROR 503! </h1>' + 
+        '<h2 id="error">We are sorry, but it seems that this website is under maintenance. Please try again later!</h2>' +
+        '<img src="Aperture_Science_logo_gray.png" alt="Aperture_Science_logo_gray" id="bottomlogo">'
+    },10000);
+    }
+};
